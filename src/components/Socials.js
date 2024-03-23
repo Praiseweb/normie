5f295e4d5f8dc23bde95543078b108c6f4225a06
praise <faderopraise18@gmail.com>
2024-03-23 13:53:32 +0100

    dd

--- src/components/Socials.js
+++ src/components/Socials.js
@@ -8,14 +8,17 @@ import Bubbles from './Bubbles'
 
 function Socials() {
   return (
-    <div className='relative bg-[#1D1D1D]'>
+    <div className='bg-[#1D1D1D]'>
       <div className="HERO flex flex-col items-center p-4 lg:p-10 relative">
         <img src={social} alt="" className='pb-10'/>
         <img src={jointhenormiecommunity} alt="" />
       </div>
       
       <div className=' flex flex-row justify-center pt-10 lg:pt-0'>
+        
+        <a href="https://t.me/NORMIETAKEOVER" target="_blank" rel="noopener noreferrer">
         <img src={twitter} alt="" className='px-5'/>
+        </a>
         <img src={telegram} alt="" className='px-5'/>
       </div>
 
